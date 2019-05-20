2b8fe5c3194f1856cc2b8eb69a9f12346678ecf3
luic95 <44306211+luisrc95@users.noreply.github.com>
2019-05-20 14:26:30 -0500

    Updated Files

---
+++ proj0/Body.java
@@ -0,0 +1,27 @@
+public class Body{
+    double xxPos;
+    double yyPos;
+    double xxVel;
+    double yyVel;
+    double mass;
+    String imgFileName;
+
+    public Body(double xP, double yP, double xV, double yV, double m, String img){
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    public Body(Body b){
+        xxPos = b.xxPos;
+        yyPos = b.yyPos;
+        xxVel = b.xxVel;
+        yyVel = b.yyVel;
+        mass = b.mass;
+        imgFileName = b.imgFileName;
+    }
+
+
+}
