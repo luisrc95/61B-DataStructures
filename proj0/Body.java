4ee7e2ed3a4315c00a244b6b597aae6923c260da
luic95 <44306211+luisrc95@users.noreply.github.com>
2019-05-31 16:07:09 -0500

    Finished some of the functions

--- proj0/Body.java
+++ proj0/Body.java
@@ -22,6 +22,57 @@ public class Body{
         mass = b.mass;
         imgFileName = b.imgFileName;
     }
+    public double calcDistance(Body b1){
+        double dx = Math.abs(this.xxPos - b1.xxPos);
+        double dy = Math.abs(this.yyPos - b1.yyPos);
+        dx = dx*dx;
+        dy = dy*dy;
 
+        double r = dx + dy;
+        r = Math.sqrt(r);
+
+        return r;
+    }
+    public double calcForceExertedBy(Body b1){
+        double G = 6.67e-11;// Nm^2/kg^2
+        double force = (G * this.mass * b1.mass)/Math.pow(this.calcDistance(b1),2);
+
+        return force;
+    }
+
+   public double calcForceExertedByX(Body b1){
+        double force = this.calcForceExertedBy(b1) * (b1.xxPos - this.xxPos);    
+        force = force / this.calcDistance(b1);
+        return force; 
+    }
+
+    public double calcForceExertedByY(Body b1){
+        double force = this.calcForceExertedBy(b1) * (b1.yyPos - this.yyPos);    
+        force = force / this.calcDistance(b1);
+        return force; 
+    }
+
+    public double calcNetForceExertedByX(Body[] b1){
+        double force = 0;
+
+        for(int i =0; i < b1.length; i++){
+            if(b1[i].equals(this))continue;
+            force += this.calcForceExertedByX(b1[i]);
+        }
+
+        return force;
+    }
+
+
+    public double calcNetForceExertedByY(Body[] b1){
+        double force = 0;
+
+        for(int i =0; i < b1.length; i++){
+            if(b1[i].equals(this))continue;
+            force += this.calcForceExertedByY(b1[i]);
+        }
+
+        return force;
+    }
 
 }
