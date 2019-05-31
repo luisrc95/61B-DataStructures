19878cf68cc7bb35876b5375a98320bb3c93b013
luic95 <44306211+luisrc95@users.noreply.github.com>
2019-05-31 14:16:13 -0500

    Revert "Modifications"
    This reverts commit dfb02c3e76047ab38c7239473d9156dcafd704ad.

--- proj0/Body.java
+++ proj0/Body.java
@@ -22,30 +22,6 @@ public class Body{
         mass = b.mass;
         imgFileName = b.imgFileName;
     }
-    public double calcDistance(Body b1){
-        double dx = Math.abs(this.xxPos - b1.xxPos);
-        double dy = Math.abs(this.yyPos - b1.yyPos);
-        dx = dx*dx;
-        dy = dy*dy;
-
-        double r = dx + dy;
-        r = Math.sqrt(r);
-
-        return r;
-    }
-    public double calcForceExertedBy(Body b1){
-        double G = 6.67e-11;// Nm^2/kg^2
-        double fource = (G * this.mass * b1.mass)/Math.sqrt(this.calcDistance(b1));
-
-        return fource; 
-    }
-    public double calcNetForceExertedByX(Body bodies[]){
-        double xCount = 0;
-        for(int i = 0; i < bodies.length; i++){
-            xCount += calcForceExertedBy(bodies[i]) * Math.abs(this.xxPos - bodies[i].xxPos);
-        }
-        return xCount;
-    }
 
 
 }
